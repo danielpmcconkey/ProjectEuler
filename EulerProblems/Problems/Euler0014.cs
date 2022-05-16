c13e5345781a93a6fe89e8f905d2a076894100a0
danielpmcconkey <danielpmcconkey@gmail.com>
2022-05-16 10:32:11 -0400

    problem 14 solved

---
+++ EulerProblems/Problems/Euler0014.cs
@@ -0,0 +1,65 @@
+﻿using EulerProblems.Lib;
+using System.Text.RegularExpressions;
+
+namespace EulerProblems.Problems
+{
+    internal class Euler0014 : Euler
+    {
+        public Euler0014() : base()
+        {
+            title = "Longest Collatz sequence";
+            problemNumber = 14;
+            PrintTitle();
+        }
+        public override void Run()
+        {
+            long longestChain = 0;
+            int longestChainStartingValue = 0;
+            int stoppingPoint = 1000000;
+            int startingPoint = 10;
+
+            for(int i = startingPoint; i <= stoppingPoint; i++)
+            {
+                //if(i == 113383)
+                //{
+                //    string burp = "true";
+                //}
+                long chainLength = HowLongIsTheCollatzChainForN(i);
+                if(chainLength > longestChain)
+                {
+                    longestChain = chainLength;
+                    longestChainStartingValue = i;
+                }
+            }
+
+
+
+            PrintSolution(longestChainStartingValue.ToString());
+            return;
+        }
+        private long HowLongIsTheCollatzChainForN(int n)
+        {
+            long currentVal = n;
+            long chainLength = 1;
+            while (currentVal != 1)
+            {
+                // if currentVal is even, divide by 2
+                // if currentVal is odd, muliply by 3 and add 1
+                if(currentVal % 2 == 0)
+                {
+                    currentVal = currentVal / 2;
+                }
+                else
+                {
+                    currentVal = (currentVal * 3) + 1;
+                }
+                chainLength++;
+                //if (n == 113383)
+                //{
+                //    Console.WriteLine(n.ToString());
+                //}
+            }
+            return chainLength;
+        }
+    }
+}
--- EulerProblems/Program.cs
+++ EulerProblems/Program.cs
@@ -3,7 +3,7 @@ using EulerProblems.Problems;
 using System.Diagnostics;
 
 
-var euler = new Euler0013();
+var euler = new Euler0014();
 euler.Run();
 euler.Deconstructor();
 
