85b0d62b4e2bb17e3d2ff7a58d81c78c41ce471c
danielpmcconkey <danielpmcconkey@gmail.com>
2022-05-17 12:52:56 -0400

    problem 19 solved

---
+++ EulerProblems/Problems/Euler0019.cs
@@ -0,0 +1,31 @@
+﻿using EulerProblems.Lib;
+using System.Text.RegularExpressions;
+
+namespace EulerProblems.Problems
+{
+    internal class Euler0019 : Euler
+    {
+        public Euler0019() : base()
+        {
+            title = "Counting Sundays";
+            problemNumber = 19;
+            PrintTitle();
+        }
+        public override void Run()
+        {
+            var startDate = DateTime.Parse("Jan 1, 1901");  // inclusive
+            var endDate = DateTime.Parse("Jan 1, 2001");    // exclusive
+            var thisDate = startDate;
+            int sundays = 0;
+            while(thisDate < endDate)
+            {
+                if(thisDate.DayOfWeek == DayOfWeek.Sunday) sundays++;
+                thisDate = thisDate.AddMonths(1);
+            }
+
+            PrintSolution(sundays.ToString());
+            return;
+        }
+
+    }
+}
