2775709e900dff4ab0411ef6d3a7a0624ef421bc
danielpmcconkey <danielpmcconkey@gmail.com>
2022-05-29 20:55:28 -0400

    problem 48 but more efficient

--- Lib/Problems/Euler0048.cs
+++ Lib/Problems/Euler0048.cs
@@ -1,4 +1,6 @@
 //#define VERBOSEOUTPUT
+using System.Numerics;
+
 namespace EulerProblems.Lib.Problems
 {
 	public class Euler0048 : Euler
@@ -9,7 +11,29 @@ namespace EulerProblems.Lib.Problems
 			problemNumber = 48;
 		}
 		protected override void Run()
+        {
+			// Run_bruteForce();
+			Run_bruteForceish();
+
+		}
+		protected void Run_bruteForceish()
 		{
+			// this only takes 32 milliseconds. I guess my BigCalculator is bogus
+			int limit = 999; // 1000^1000 is only going to add 0s to the last 10 digits
+			BigInteger sum = 0;
+
+			for (int i = 1; i <= limit; i++)
+			{
+				sum += BigInteger.Pow(i, i);
+			}
+			string sumAsString = sum.ToString();
+			string answer = sumAsString.Substring(sumAsString.Length - 10);
+			PrintSolution(answer);
+			return;
+		}
+		protected void Run_bruteForce()
+		{
+			// this way take about 2 minutes to run. gots to be a better way
 			int limit = 1000;
 			BigNumber sum = new BigNumber(0);
 
