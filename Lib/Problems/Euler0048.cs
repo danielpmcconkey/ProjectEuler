d48085c318314ac63b6eb5e7fb9302b1d580466d
danielpmcconkey <danielpmcconkey@gmail.com>
2022-05-29 20:35:33 -0400

    problem 48 solved

--- Lib/Problems/Euler0048.cs
+++ Lib/Problems/Euler0048.cs
@@ -5,13 +5,21 @@ namespace EulerProblems.Lib.Problems
 	{
 		public Euler0048() : base()
 		{
-			title = "Template";
+			title = "Self powers";
 			problemNumber = 48;
 		}
 		protected override void Run()
 		{
-			int answer = 0;
-			PrintSolution(answer.ToString());
+			int limit = 1000;
+			BigNumber sum = new BigNumber(0);
+
+			for(int i = 1; i <= limit; i++)
+            {
+				BigNumber exponentResult = BigNumberCalculator.Exponent(i, i);
+				sum = BigNumberCalculator.Add(sum, exponentResult);
+            }
+			
+			PrintSolution(sum.ToString());
 			return;
 		}
 	}
