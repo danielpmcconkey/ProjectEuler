419c5f943a190710c55e950d988aa544dde40e4b
danielpmcconkey <danielpmcconkey@gmail.com>
2022-07-10 10:57:28 -0400

    problem 5 in f#

--- EulerCLI/EulerCLI.cs
+++ EulerCLI/EulerCLI.cs
@@ -13,7 +13,7 @@ using EulerProblems.Lib.DAL.Operations;
 //TestHelper.BenchMarkNewSolutions();
 
 
-var euler = EulerProblemFactory.GetEulerProblemClassByNumber(90);
+var euler = EulerProblemFactory.GetEulerProblemClassByNumber(5);
 euler.Solve();
 
 
---
+++ EulerFunctionalCLI/Problems/Euler005.fs
@@ -0,0 +1,25 @@
+﻿module Euler005
+
+
+let run () =
+
+    
+    let denominators = { 11 .. 19 } |> Seq.rev
+    let numerators = { 20 .. 20 .. 300000000 }
+
+    let canMod n m = 
+        if n % m = 0 then true else false
+
+    let isASolution n =         
+        denominators
+        |> Seq.forall (canMod n)
+        
+
+    let minSolution = 
+        numerators
+        |> Seq.find isASolution
+        //|> Seq.min
+        
+    minSolution.ToString()
+    
+  
