974c60bc5a2adc21f73d165051fc24ac656092af
danielpmcconkey <danielpmcconkey@gmail.com>
2022-08-01 20:23:43 -0400

    problem 48 in F#

--- EulerCLI/EulerCLI.cs
+++ EulerCLI/EulerCLI.cs
@@ -13,7 +13,7 @@ using EulerProblems.Lib.DAL.Operations;
 //TestHelper.BenchMarkNewSolutions();
 
 
-var euler = EulerProblemFactory.GetEulerProblemClassByNumber(46);
+var euler = EulerProblemFactory.GetEulerProblemClassByNumber(48);
 euler.Solve();
 
 
---
+++ EulerFunctionalCLI/Problems/Euler048.fs
@@ -0,0 +1,21 @@
+﻿module Euler048
+
+let run () =
+
+    (*
+    So, instead of utilizing the BigInteger class and just boring my way 
+    through all the arithemetic, I used a modulo to "clip" the answer at 10 
+    characters each step of the way. It runs lickety split.
+
+    Lickety.
+    *)
+
+    let longToPaddedString (n:int64) = n.ToString("0000000000")
+    let addAndCrop (l1: int64) (l2: int64) = (l1 + l2) % (int64) 1e10
+    let multiplyAndCrop (l1: int64) (l2: int64) = (l1 * l2) % (int64) 1e10
+    let raiseToSelf i = 
+        Array.create i i 
+        |> Array.fold (fun acc elem -> multiplyAndCrop ((int64)acc)  ((int64)elem)) 1L
+    [|1..1000|] 
+    |> Array.fold (fun acc elem -> addAndCrop acc (raiseToSelf elem)) 0L
+    |> longToPaddedString
