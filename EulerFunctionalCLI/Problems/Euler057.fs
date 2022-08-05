408e01777a60b2e289cc696faec9db079964aa82
danielpmcconkey <danielpmcconkey@gmail.com>
2022-08-05 18:57:13 -0400

    57 in F#

---
+++ EulerFunctionalCLI/Problems/Euler057.fs
@@ -0,0 +1,25 @@
+﻿module Euler057
+open System.Numerics
+open Conversions
+let run () =
+    (*
+    How does this work? Well the fractions shown in the problem. 3/2, 7/5, 
+    17/12...The follow the pattern of new denominator = previous numerator + 
+    previous denominator and the new numerator is the old numerator + 2 * the 
+    old denominator. So you can extend that series. And you also don't need to 
+    actually count the digits. If you take a log 10, that'll tell you the 
+    number of digits (so long as you round down).
+    *)
+    let limit = 1000
+    let answerT = 
+        [|1..limit|]
+        |> Array.fold (fun acc elem -> 
+            let numerator, denominator, answer = acc
+            let newDen = numerator + denominator
+            let newNum = (2I * denominator) + numerator
+            if (int)(BigInteger.Log10 numerator) > (int)(BigInteger.Log10 denominator)
+                then (newNum, newDen, answer + 1)
+                else (newNum, newDen, answer)
+        ) (3I, 2I, 0)
+    let numerator, denominator, answer = answerT
+    answer |> intToString
--- EulerFunctionalCLI/Program.fs
+++ EulerFunctionalCLI/Program.fs
@@ -1,7 +1,7 @@
 ﻿let stopWatch = System.Diagnostics.Stopwatch()
 stopWatch.Start()
 
-let (answer : string) = Euler056.run() 
+let (answer : string) = Euler057.run() 
 
 stopWatch.Stop()
 printfn "answer is: %s" answer
