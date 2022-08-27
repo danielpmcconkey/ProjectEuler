b029230e1c278a9c0446acf4daded88db7f0f5ff
danielpmcconkey <danielpmcconkey@gmail.com>
2022-08-27 10:54:32 -0400

    75 in F#

---
+++ EulerFunctionalCLI/Problems/Euler075.fs
@@ -0,0 +1,17 @@
+﻿module Euler075
+
+open Algorithms
+open Conversions
+
+let run () =
+    (*
+    This is the exact same problem as 39 but with a bigger max perimeter. The 
+    code is almost identical.
+    *)
+    let maxPerimeter = (int)1.5e6
+
+    getPythagoreanTriangles maxPerimeter 
+    |> List.groupBy (fun t -> t.perimeter)
+    |> List.filter (fun g -> (snd g).Length = 1)
+    |> List.length
+    |> intToString
--- EulerFunctionalCLI/Program.fs
+++ EulerFunctionalCLI/Program.fs
@@ -1,7 +1,7 @@
 ﻿let stopWatch = System.Diagnostics.Stopwatch()
 stopWatch.Start()
 
-let (answer : string) = Euler074.run() 
+let (answer : string) = Euler075.run() 
 
 stopWatch.Stop()
 printfn "answer is: %s" answer
