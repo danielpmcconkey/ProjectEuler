99feb64ea04a33891b96ccf7f719cf06a4f5f1d6
danielpmcconkey <danielpmcconkey@gmail.com>
2022-09-08 09:51:48 -0400

    85 in F#

---
+++ EulerFunctionalCLI/Problems/Euler085.fs
@@ -0,0 +1,19 @@
+﻿module Euler085
+open Conversions
+
+let run () =
+    let triangleOfN n = n * (n + 1) / 2
+    let triangleMN m n = triangleOfN m * triangleOfN n
+    let diff target m n = abs (target - triangleMN m n)
+    let target = (int)2e6
+    let minSide = 30
+    let maxSide = 90
+    [|minSide..maxSide|]
+    |> Array.collect (fun m -> 
+        [|m..maxSide|]
+        |> Array.map (fun n -> (m, n))
+        )
+    |> Array.map (fun (m, n) -> (m, n, diff target m n))
+    |> Array.minBy (fun (m, n, difference) -> difference)
+    |> (fun (m, n, difference) -> m * n)
+    |> intToString
